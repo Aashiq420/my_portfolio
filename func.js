9e7e59102286195e1c47824c80ed8d0ebbf32d71
Aashiq <adams.aashiq@gmail.com>
2020-12-17 11:20:39 +0200

    second day work

--- func.js
+++ func.js
@@ -5,3 +5,14 @@ function openMenu() {
 function closeMenu() {
   document.getElementById("sideBar").style.width = "0";
 }
+
+var i = 0;
+var txt = "Hello There! \nMy name is Aashiq \nWelcome to my \nonline portfolio";
+var spd = 100;
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("typed").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, spd);
+  }
+}
